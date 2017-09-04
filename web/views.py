141010254de2b3380588e48acef95be0cbc82d2a
Maxim H <amigo00678@gmail.com>
2017-09-04 16:50:43 +0400

    Added list

--- web/views.py
+++ web/views.py
@@ -14,9 +14,9 @@ class FilesListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(FilesListView, self).get_context_data(**kwargs)
         context['files'] = [
-            {'name': 'f1'},
-            {'name': 'f2'},
-            {'name': 'f3'},
+            {'name': 'f1', 'created_at': datetime.now(), 'type': 3},
+            {'name': 'f2', 'created_at': datetime.now(), 'type': 3},
+            {'name': 'f3', 'created_at': datetime.now(), 'type': 3},
         ]
         return context
 
