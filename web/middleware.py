0d4fa550ba3813f92db735217ee019e499734e21
marian.holovatskyy@stratatech.ru <marian.holovatskyy@stratatech.ru>
2018-05-14 15:23:56 +0400

    add session variables

--- web/middleware.py
+++ web/middleware.py
@@ -4,6 +4,12 @@ from web.constants import *
 class DataMiddleware(object):
 
     def process_request(self, request):
-        if request.method == 'POST' and 'start_date' in request.POST and 'end_date' in request.POST:
-            request.session['start_date'] = request.POST.get('start_date')
-            request.session['end_date'] = request.POST.get('end_date')
+        if request.method == 'POST':
+            if 'start_date' in request.POST:
+                request.session['start_date'] = request.POST.get('start_date')
+            if 'end_date' in request.POST:
+                request.session['end_date'] = request.POST.get('end_date')
+            if 'guest_number' in request.POST:
+                request.session['guest_number'] = request.POST.get('guest_number')
+            if 'rooms_number' in request.POST:
+                request.session['rooms_number'] = request.POST.get('rooms_number')
